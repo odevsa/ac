5222181f683146633099426af95e2acdcf9d19be
Odevsa <dev.raphaelsa@gmail.com>
2026-03-22 22:12:43 -0300

    Enhance pacman customization logic for ILoveCandy setting

--- tasks/pacman.sh
+++ tasks/pacman.sh
@@ -21,9 +21,13 @@ sudo systemctl enable --now reflector.service reflector.timer || true
 # Customize pacman
 # ###########################################################
 echo "=> Customizing pacman..."
-sudo sed -i -E 's/^[[:space:]]*#[[:space:]]*(Color.*)$/\1/' /etc/pacman.conf || true
-sudo sed -i -E 's/^[[:space:]]*#[[:space:]]*(ILoveCandy.*)$/\1/' /etc/pacman.conf || true
 sudo sed -i -E 's/^[[:space:]]*#?[[:space:]]*ParallelDownloads.*$/ParallelDownloads = 10/' /etc/pacman.conf || true
+sudo sed -i -E 's/^[[:space:]]*#[[:space:]]*(Color.*)$/\1/' /etc/pacman.conf || true
+if sudo grep -Eq '^[[:space:]]*#?[[:space:]]*ILoveCandy' /etc/pacman.conf; then
+  sudo sed -i -E 's/^[[:space:]]*#[[:space:]]*(ILoveCandy.*)$/\1/' /etc/pacman.conf || true
+else
+  sudo sed -i -E '/^[[:space:]]*Color/ a ILoveCandy' /etc/pacman.conf || true
+fi
 
 # ###########################################################
 # General packages
